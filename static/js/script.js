bba6bd9f827a6358315e6eaa06377638ae3bf6c8
Sadegh Movahed <sadeqmail136@gmail.com>
2025-08-25 21:19:40 +0330

    Update portfolio

--- static/js/script.js
+++ static/js/script.js
@@ -66,7 +66,7 @@ menuIcon.onclick = () => {
 document.addEventListener("DOMContentLoaded", function(){
     const form = document.getElementById('contact-form');
         form.addEventListener('submit', function(e) {
-            e.preventDefault();
+            // e.preventDefault();
             const name = document.getElementById('name').value;
             const email = document.getElementById('email').value;
             const message = document.getElementById('message').value;
